645c1e067a2938372707ca1f364ee81c761ea942
vignesh010215 <vignesh.r0607@gmail.com>
2025-07-29 18:30:09 +0530

    correct the code

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   const msg = 'Hello Node!\n'
   res.end(msg);
-#});:
+});
 
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
